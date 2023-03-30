744a7d2435ddee7e74c56aa00f9406030b6f0efb
unknown <kdiniz732@gmail.com>
2023-03-30 09:12:58 -0300

    adicionando novo arquivo index.js e variaveis.js

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(1); //n<umber
+console.log("Kaio"); //string <- palavras, letras e numeros entre aspas
+console.log ("1");
+console.log(18 + 1 * 10);
+
+//number inteiros = int
+// decimal = float
+
+console.log("Sou Kaio, tenho", 18, "anos");
---
+++ variaveis.js
@@ -0,0 +1,27 @@
+ //assim q se escreve uma variavel camelCase
+ //case sensitivity
+
+
+
+// calcula imc
+// nome completo, idade, peso altura
+// imc -> peso / (altura * alto) *formula*
+// descobrir ano de ano de nascimento
+
+const nome1 = 'Kaio'
+
+const nome2 = 'Diniz'
+
+const nome3 = 'dos'
+
+const nome4 = 'Santos'
+
+let idade = 18
+
+let peso = 52.2
+
+let altura = 1.72
+console.log(nome1, nome2, nome3, nome4, idade, peso, altura)
+let imc;
+imc = peso / (altura * altura)
+ console.log(imc)
